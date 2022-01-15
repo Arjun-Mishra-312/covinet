f9f12a9f8dd93035d6ebca52345978dc9438507f
Arjun-Mishra-312 <arjunmishra31204@gmail.com>
2022-01-15 22:14:09 +0530

    Messaging and news

--- lib/Screens/MainScreen.dart
+++ lib/Screens/MainScreen.dart
@@ -1,3 +1,5 @@
+import 'package:covinet/News/news.dart';
+import 'package:covinet/Screens/MessageScreen.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -9,17 +11,44 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("main screen"),
       ),
-      body: GestureDetector(
-        onTap: () {
-        },
-        child: Center(
-          child: Container(
-            decoration: BoxDecoration(border: Border.all(color: Colors.black), borderRadius: BorderRadius.circular(20)),
-            height: 200,
-            width: 200,
-            child: Center(child: Text("Messaging")),
+      body: Column(
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (_) => MessageScreen()));
+            },
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(20)),
+                height: 200,
+                width: 200,
+                child: Center(child: Text("Messaging")),
+              ),
+            ),
           ),
-        ),
+          SizedBox(
+            height: 200,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (_) => NewsScreen()));
+            },
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(20)),
+                height: 200,
+                width: 200,
+                child: Center(child: Text("NEWS")),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
